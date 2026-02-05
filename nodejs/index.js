ef9b7c4ae473c93619ed69af16e6852556fdac8f
Lavish0007 <patellavish.9336@gmail.com>
2026-02-05 11:52:01 +0530

    done

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,30 +1,44 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
 
-const myserver = http.createServer((req, res) => {
+// const myserver = http.createServer((req, res) => {
 
-    if (req.url === '/') {
+//     if (req.url === '/') {
 
-        const imgPath = path.join(__dirname, 'Images', 'bool.jpg');
-        const img = fs.readFileSync(imgPath);
-        const base64Image = img.toString('base64');
+//         const imgPath = path.join(__dirname, 'Images', 'bool.jpg');
+//         const img = fs.readFileSync(imgPath);
+//         const base64Image = img.toString('base64');
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`
-            <h2>This is the About us page</h2>
-            <img src="data:image/jpeg;base64,${base64Image}" width="800" />
-        `);
-    }
-    else if (req.url === '/about') {
-        res.end("we are student");
-    }
-    else if (req.url === '/class') {
-        res.end("CE");
-    }
-    else {
-        res.end("404 page is not found");
-    }
-});
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.end(`
+//             <h2>This is the About us page</h2>
+//             <img src="data:image/jpeg;base64,${base64Image}" width="800" />
+//         `);
+//     }
+//     else if (req.url === '/about') {
+//         res.end("we are student");
+//     }
+//     else if (req.url === '/class') {
+//         res.end("CE");
+//     }
+//     else {
+//         res.end("404 page is not found");
+//     }
+// });
 
-myserver.listen(8000, () => console.log('server is running'));
+// myserver.listen(8000, () => console.log('server is running'));
+// fs.writeFileSync("./class.txt","Class is commencing");
+// let result=fs.readFileSync("./class.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("error",err);
+//     }
+//     else{
+//         console.log(result)
+//     }
+
+// });
+// console.log(result)
+
+const fs=require('fs');
+fs.cpSync("class.txt","copy.txt")
